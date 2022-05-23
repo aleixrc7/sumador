b173ab47885a9cb3620f8849dd52d53b74bcade5
aleixrc7 <aleixasir@gmail.com>
2022-05-23 19:28:42 +0200

    feat: clase con iniciales implementada.

---
+++ src/sumador/ArcFunciona.java
@@ -0,0 +1,11 @@
+package sumador;
+
+public class ArcFunciona {
+
+	public static void main(String[] args) {
+		ASumar sumi = new ASumar();
+		
+		System.out.println("La suma de las cifras de 11 es de " + sumi.total("11") + " Digitos");
+	}
+
+}
